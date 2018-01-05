5c25dc28610883dbe481bb74578d3b7a6d09b316
Francisco Godinho <frgo@gmv.es>
2018-01-05 03:02:38 +0000

    added https

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require("express");
+var https = require('https');
 var app = express();
 var router = express.Router();
 var path = require('path');
+var fs = require('fs');
 
-app.set('port', 3000);
+var key = fs.readFileSync('crt/fgodinho.key');
+var cert = fs.readFileSync( 'crt/fgodinho.crt' );
+var ca = fs.readFileSync( 'crt/ca.crt' );
+var options = {
+  key: key,
+  cert: cert,
+  ca: ca
+};
 
 app.use(express.static(path.join(__dirname, 'public')));
+https.createServer(options, app).listen(443);
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -21,7 +31,3 @@ app.use("/",router);
 /*app.use("*",function(req,res){
   res.sendFile(__dirname + "404.html");
 });*/
-
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
-});
